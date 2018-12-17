3c18debf8f9161ae1cdabb0df681a9a3db60c248
shifumix <dev@shifumix.com>
2018-12-17 08:11:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import classes.Corporate as Corp
+import json
+
+c=Corp.Corporate("F80")
+
+print(c.toYAML())
---
+++ venv/classes/Corporate.py
@@ -0,0 +1,19 @@
+import yaml
+import StringDist
+
+class Corporate(object):
+    name=""
+    description=""
+
+    def __init__(self,name:str):
+        self.name=name
+
+    def toYAML(self):
+        return yaml.dump(self.__dict__,default_flow_style=False)
+
+    def fromYAML(self,content:str,overwrite=False):
+        d:dict=yaml.load(str)
+        if len(name)==0 or overwrite:self.name=d.name
+        if len(description)==0 or overwrite:self.description=d.description
+
+    def update(self,html):
